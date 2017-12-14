7becc74e2f619c6a8610a12b9a1eecb85a217180
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2017-12-14 11:56:54 -0500

    Add recompose

--- src/App.js
+++ src/App.js
@@ -3,9 +3,6 @@ import MyMapComponent from './components/MyMapComponent';
 import 'semantic-ui-css/semantic.css';
 import './App.css';
 
-const googleMapURL =
-  'https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyA9CucPXf8WfL7K4HiMZzw5D8mXwrnX2XI';
-
 class App extends React.Component {
   render() {
     return (
@@ -14,13 +11,7 @@ class App extends React.Component {
           <h1 className="App-title">Google Maps Experiments</h1>
         </header>
 
-        <MyMapComponent
-          isMarkerShown
-          googleMapURL={googleMapURL}
-          loadingElement={<div style={{ height: `100%` }} />}
-          containerElement={<div style={{ height: `80vh` }} />}
-          mapElement={<div style={{ height: `100%` }} />}
-        />
+        <MyMapComponent isMarkerShown />
       </div>
     );
   }
--- src/components/MyMapComponent.js
+++ src/components/MyMapComponent.js
@@ -5,15 +5,27 @@ import {
   GoogleMap,
   Marker
 } from 'react-google-maps';
+import { compose, withProps } from 'recompose';
 
+const googleMapURL =
+  'https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyA9CucPXf8WfL7K4HiMZzw5D8mXwrnX2XI';
 const defaultCenter = { lat: 38.8977, lng: -77.0365 };
 const defaultZoom = 8;
 
 // https://tomchentw.github.io/react-google-maps/
-export default withScriptjs(
-  withGoogleMap(props => (
-    <GoogleMap defaultZoom={defaultZoom} defaultCenter={defaultCenter}>
-      {props.isMarkerShown && <Marker position={defaultCenter} />}
-    </GoogleMap>
-  ))
-);
+const MyMapComponent = compose(
+  withProps({
+    googleMapURL,
+    loadingElement: <div style={{ height: `100%` }} />,
+    containerElement: <div style={{ height: `80vh` }} />,
+    mapElement: <div style={{ height: `100%` }} />
+  }),
+  withScriptjs,
+  withGoogleMap
+)(props => (
+  <GoogleMap defaultZoom={defaultZoom} defaultCenter={defaultCenter}>
+    {props.isMarkerShown && <Marker position={defaultCenter} />}
+  </GoogleMap>
+));
+
+export default MyMapComponent;
